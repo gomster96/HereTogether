265ac1f2d81e14668c35e59402a0ff02e8bd447a
ByungwoongAn <gomster96@gmail.com>
2022-01-13 22:40:16 +0900

    feat[#1] : 이모션 적용 - close #1
    - 아래블로그를 참고하여 emotion을 적용하였습니다.
    - https://velog.io/@joabyjoa/create-react-app-typescript-emotion%EC%9C%BC%EB%A1%9C-React-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EC%8B%9C%EC%9E%91%ED%95%98%EA%B8%B0

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { css } from '@emotion/react';
+
+const myStyle = css`
+  color: red;
+`;
+
 function App() {
-  return <div>안녕하세요</div>;
+  return <div css={myStyle}>안녕하세요</div>;
 }
 
 export default App;
