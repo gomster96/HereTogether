21d051d7bf65457ae7a3793920719ff49f0c1337
안유진 <Eugenius1st@gmail.com>
2022-01-30 23:39:40 +0900

    feat[#5]: 리스트 페이지 구현 - close #5
    - 택시 리스트 레이아웃 구현
    - material UI 사용
    - background global 스타일 적용
    - 폰트적용(SDdream, localFont) -

--- front/src/components/post-list/DatePicker.tsx
+++ front/src/components/post-list/DatePicker.tsx
@@ -3,69 +3,75 @@ import { css } from '@emotion/react';
 import TextField from '@mui/material/TextField';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DatePicker from '@mui/lab/DatePicker';
 import MobileDatePicker from '@mui/lab/MobileDatePicker';
-import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
-import Stack from '@mui/material/Stack';
+
+import { makeStyles, createStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    noBorder: {
+      outline: 'none',
+      border: 'none',
+    },
+    // textColor: {
+    //   color: 'white',
+    // },
+  })
+);
 
 export default function ResponsiveDatePickers() {
   const [value, setValue] = React.useState<Date | null>(new Date());
+  const classes = useStyles();
   const color = 'ffffff';
+
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <Stack spacing={3} sx={{ border: '1px solid red' }}>
-        <MobileDatePicker
-          label="For mobile"
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              sx={{
-                backgroundColor: 'white',
-              }}
-            />
-          )}
-        />
-      </Stack>
+      <MobileDatePicker
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+        //위에 파라미터 받아오는 부분만 수정하면 된다 !!
+        InputProps={{
+          classes: { notchedOutline: classes.noBorder },
+        }}
+        //위는 되고 아래는 안되는 이유는 뭐니?
+        // css={test}
+        css={{ test }}
+      />
     </LocalizationProvider>
   );
 }
 
-// const test = css`
-//   display: none;
-//   /* background-color: red; */
-//   border: none;
-// `;
+const test = css`
+  padding: 500px;
+  border: 1px solid red;
+`;
 
-//   return (
-//     <LocalizationProvider
-//       dateAdapter={AdapterDateFns}
-//       sx={{
-//         border: 0,
-//       }}
-//     >
-//       <DesktopDatePicker
-//         label="For desktop"
-//         value={value}
-//         minDate={new Date('2017-01-01')}
-//         onChange={(newValue) => {
-//           setValue(newValue);
-//         }}
-//         renderInput={(params) => (
-//           <TextField
-//             {...params}
-//             sx={{
-//               svg: { display: 'none' },
-//               input: { color },
-//               border: '2px',
-//               padding: '30px',
-//             }}
-//           />
-//         )}
-//       />
-//     </LocalizationProvider>
-//   );
-// }
+// 방법 2
+// const theme = createTheme({
+//   components: {
+//     MuiIconButton: {
+//       styleOverrides: {
+//         sizeMedium: {
+//           color
+//         }
+//       }
+//     },
+//     MuiOutlinedInput: {
+//       styleOverrides: {
+//         root: {
+//           color
+//         }
+//       }
+//     },
+//     MuiInputLabel: {
+//       styleOverrides: {
+//         root: {
+//           color
+//         }
+//       }
+//     }
+//   }
+// });
--- frontend/src/type/index.ts
+++
@@ -1,9 +0,0 @@
-export type ItemType = {
-  title: string;
-  time: string;
-  start: string;
-  destination: string;
-  content: string;
-  participant: number;
-  totalPeople: number;
-};
